e31b8621255e8d0c5c0f7f46eeb74bd8f4fb37a8
Peregrine Badger <pbadger27@gmail.com>
2015-07-07 10:30:26 -0400

    maybe adding nfc to index.js works?

--- js/index.js
+++ js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        console.log('this! ->> ', nfc)
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
